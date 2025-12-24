7d269b6f239afc8cfbe07424d8178d58dc4d7007
house <house_shores.5x@icloud.com>
2025-12-24 19:26:57 +0800

    init lesson2

--- lesson1/lesson1.md
+++
@@ -1,6 +0,0 @@
-# 如何创建一个Vue.js
-- cdn
-- vue cli
-- create vue
-- nuxt.js或者gridsome
-¥ cdn
--- lesson2-use-vue-locally/vite.config.js
+++ lesson2-use-vue-locally/vite.config.js
@@ -10,9 +10,6 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  server:{
-    port:3000
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
