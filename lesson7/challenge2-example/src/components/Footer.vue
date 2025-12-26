1d2bdf03ab98daa4ee4f7f3f48e47984a833307b
house <house_shores.5x@icloud.com>
2025-12-26 18:42:10 +0800

    add lesson7

---
+++ lesson7/challenge2-example/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<script setup>
+	
+</script>
+
+<template>
+	<footer>
+		<p>Learn more about me at <a target="_blank" href="https://vuejs.org/">vuejs.org</a></p>
+	</footer>
+</template>
+
+<style>
+	/* FOOTER CSS */
+	footer {
+	margin-top: 35px;
+	font-size: 0.8rem;
+	}
+
+	footer a {
+	color: #40b883;
+	}
+</style>
