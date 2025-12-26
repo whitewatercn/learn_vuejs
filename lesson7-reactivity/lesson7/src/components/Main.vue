d0d5f8c84949af5a77cbb1047911089c2b02fd3b
house <house_shores.5x@icloud.com>
2025-12-26 18:57:36 +0800

    add lesson7

---
+++ lesson7-reactivity/lesson7/src/components/Main.vue
@@ -0,0 +1,76 @@
+<script setup>
+	import { ref } from 'vue'
+	const quote = ref('First, solve the problem. Then, write the code.')
+	const author = ref('John Johnson')
+</script>
+
+<template>
+  <main>
+      <section>
+          <p>{{ quote }}</p>
+          <span>{{ author }}</span>
+      </section>
+      <button>Another!</button>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    background: #CCD6D9;
+    max-width:90%;
+    border-radius:0 15px 0 15px;
+    padding:35px;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    gap:10px;
+  }
+
+  section {
+      display: flex;
+      width:100%;
+      flex-direction:column;
+      gap:10px;
+      margin-bottom:25px;
+  }
+
+  p {
+      font-weight:bold;
+      font-style: italic;
+      font-size:2rem;
+      text-align:left;
+  }
+
+  p::before {
+      content: '"'
+  }
+
+  p::after {
+      content: '"'
+  }
+
+  span {
+      align-self:end;
+      color: #406473;
+  }
+
+  span::before {
+      content: "- "
+  }
+
+  button {
+      background:#406473;
+      color: white;
+      padding:10px;
+      border: 0;
+      font-size:1.2rem;
+      border-radius:0 5px 0 5px;
+      font-weight:bold;
+      margin-top:20px;
+      cursor:pointer;
+      transition: transform 0.2s;
+  }
+  button:hover{
+      transform: scale(1.05);
+  }
+</style>
---
+++ lesson8-arribute-binding/lesson7/src/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup>
+	import { ref } from 'vue';
+	const title = ref('Quote generator');
+// 你可以直接改变title的值，新的变量会覆盖旧的变量，vuejs会自动解析最新的ref.value
+	// title.value = "hello world";
+</script>
+
+<template>
+  <header>
+      <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<style scoped>
+  header {
+      color:#CCD6D9;
+      margin-bottom:25px;
+  }
+</style>
