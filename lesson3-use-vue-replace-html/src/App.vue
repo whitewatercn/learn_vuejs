596e95fec4a99bdadacaa73fba9221e80edeea8f
house <house_shores.5x@icloud.com>
2025-12-26 11:43:22 +0800

    lesson3

---
+++ lesson3-use-vue-replace-html/src/App.vue
@@ -0,0 +1,45 @@
+<!-- 通过这个app.vue，我们通过Vue component实现了lesson2里的html -->
+
+<!-- script放置函数 -->
+<script setup>
+  import {ref} from 'vue'
+  const name = ref('doctor')
+  const emoji = ref('😊')
+</script>
+
+
+<!-- html里的body放在了Vue里的template -->
+<template>
+  <header>
+    <span>{{ emoji }}</span>
+  </header>
+
+  <main>
+    <h1>hello, {{ name }}</h1>
+    <p>let's learn vuejs</p>
+  </main>
+
+  <footer>
+    <p>&copy; 2025 beginner
+    </p>
+  </footer>
+</template>
+
+<!-- style用来设置样式 -->
+<style scoped>
+  header span{
+    font-size: 3rem;
+  }
+  main h1{
+    display:block;
+  }
+
+  main p{
+    /* color:beige; */
+  }
+
+  footer p{
+    font-size:0.8rem;
+    color:red;
+  }
+</style>
---
+++ lesson3-use-vue-replace-html/vite.config.js
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+// 暂时用不到 vue-devtools 插件，先注释掉
+// import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    // vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
