90b644355e2cefaa372d86c7c229f93e100e6390
house <house_shores.5x@icloud.com>
2025-12-29 11:50:37 +0800

    add lesson8

---
+++ lesson9-more-about-vbind/lesson9-example/src/components/Footer.vue
@@ -0,0 +1,40 @@
+<script setup>
+	import { ref } from 'vue';
+	const creator = ref('Beginner Center');
+	const year = ref(2025);
+  const href = ref('https://forum.beginner.center/')
+</script>
+
+<template>
+  <footer>
+    <!-- 
+    通过v-bind来实现超链接功能 
+    mustache{{  }} 语法只会渲染为文本内容，不能改变html属性值
+    v-bind:属性名="变量名"
+    
+    -->
+     
+      Created by <a v-bind:href="href"><span>{{ creator }}</span></a> &copy; {{ year }}
+      <!-- Created by <a href="{{href}}"><span>{{ creator }}</span></a> &copy; {{ year }} -->
+
+  </footer>
+</template>
+
+<style scoped>
+  footer {
+    position:fixed;
+    margin: 0 auto;
+    bottom:0;
+    text-align:center;
+    color:#CCD6D9;
+    padding-bottom:20px;
+  }
+
+  span{
+      font-family: cursive;
+  }
+  a{
+      color:#64FFDA;
+      /* text-decoration:none; */
+  }
+</style>
---
+++ lesson9-more-about-vbind/lesson9-example/src/components/Header.vue
@@ -0,0 +1,20 @@
+<script setup>
+	import { ref } from 'vue';
+	const title = ref('Quote generator');
+// title是一个ref对象，这个ref有很多属性，比如值value
+// 你可以直接改变title这个ref的值，新的变量会覆盖旧的变量，vuejs会自动解析最新的ref.value
+	// title.value = "hello world";
+</script>
+
+<template>
+  <header>
+      <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<style scoped>
+  header {
+      color:#CCD6D9;
+      margin-bottom:25px;
+  }
+</style>
