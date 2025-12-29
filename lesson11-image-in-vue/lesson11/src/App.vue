a36f350f40edd510698736c3737dc8bbada3ac12
house <house_shores.5x@icloud.com>
2025-12-29 16:59:27 +0800

    add lesson11

---
+++ lesson11-image-in-vue/lesson11/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import logo from '@/assets/logo.svg';
+</script>
+
+<template>
+  <section>
+    <img src = '/favicon.ico' alt="Vue logo"/>
+    <img src = '/logo.svg' alt="Vue logo"/>
+  </section>
+
+  <section>
+    <img src = '@/assets/logo.svg' alt="Vue logo"/>
+  </section>
+  
+  <section>
+    <img v-bind:src="logo" alt = "Vue logo"/>
+  </section>
+</template>
+
+<style scoped>
+  section {
+    display:grid;
+    grid-template-columns:1fr 1fr;
+    padding:10px;
+    background:white;
+    border-radius:10px;
+  }
+  img{
+    width:100%;
+  }
+</style>
